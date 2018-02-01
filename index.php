3146e16cdb02b409485cae8c08595cdd02a5f665
MZaid <mzaidr9@gmail.com>
2018-02-02 02:26:58 +0530

    change msg

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Check that out! We changed something!</h1>";
+echo "<h1>Check that out! We changed to CI, yay!</h1>";
 echo "<p>You've just created your first Web App for Containers app!</p>";
 ?>
